55a04387aee8ccba452a3164083012806d6ccf2e
pedroduarte <p.fmarket1996@gmail.com>
2022-10-11 16:59:10 -0300

    Implementando

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,35 @@
 package application;
 
+import model.entities.Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) throws ParseException {
+        List<Reservation> list = new ArrayList<>();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o numero do quanto: ");
+        int roomNumber = sc.nextInt();
+
+        System.out.println("Data de Check-In (DD/MM/AAAA)");
+        Date checkin = sdf.parse(sc.next());
+
+        System.out.println("Data de Check-Out (DD/MM/AAAA)");
+        Date checkout = sdf.parse(sc.next());
+        
+        Reservation reservation = new Reservation(roomNumber, checkin,checkout);
+
+        System.out.println(reservation);
+
+        sc.close();
+    }
 }
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,2 +1,67 @@
-package model.entities;public class Reservation {
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+    public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    private Integer roomNumber;
+
+    private Date checkin;
+
+    private Date checkout;
+
+
+
+    public Reservation(){
+
+    }
+
+    public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+        this.roomNumber = roomNumber;
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckin() {
+        return checkin;
+    }
+
+
+    public Date getCheckout() {
+        return checkout;
+    }
+
+
+
+    /*Método para calcular a duração do tempo entre o checkin e o checkout,
+     primeiro é criada uma variável para pegar o tempo do checkin e checkout em milisegundos com um getTime
+     depois é feita uma conversão desses milisegundos para dias nessa caso, mas em outros casos pode ser feito para outros formatos */
+    public long duration(){
+        long diferença = checkout.getTime() - checkin.getTime();
+        return TimeUnit.DAYS.convert(diferença, TimeUnit.MILLISECONDS);
+    }
+    public void updateDates(Date checkin, Date checkout){
+        this.checkin = checkin;
+        this.checkout = checkout;
+
+    }
+
+    @Override
+    public String toString() {
+        return  "roomNumber " + roomNumber +
+                ", checkin  " + checkin +
+                ", checkout " + checkout +
+                ", Duration " + sdf.format(duration()) +
+                " days";
+    }
 }
